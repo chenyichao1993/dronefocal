e5430c7ca7b1e8505ef07135cffdd57d4c40af87
chenyichao1993 <626697455@qq.com>
2025-11-02 13:11:09 +0800

    修复构建追踪栈溢出错误：清理.gitignore重复项并添加构建追踪排除配置

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+        'node_modules/webpack',
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
